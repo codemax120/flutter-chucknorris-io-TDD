9ea69c3cad30cbde7fed410cb675b26974e09122
Max Herrera <harvey.herrera@lumation.co>
2022-12-04 23:48:46 -0600

    added random injection dependecy with getIt

--- lib/core/injection/injection_container.dart
+++ lib/core/injection/injection_container.dart
@@ -1,5 +1,11 @@
 import 'package:chuck_norris_io/core/network/network_info.dart';
 import 'package:chuck_norris_io/core/network/server_api_client.dart';
+import 'package:chuck_norris_io/features/random/data/datasources/random_datasource.dart';
+import 'package:chuck_norris_io/features/random/data/datasources/random_datasource_impl.dart';
+import 'package:chuck_norris_io/features/random/data/repositories/random_repository_impl.dart';
+import 'package:chuck_norris_io/features/random/domain/repositories/random_repository.dart';
+import 'package:chuck_norris_io/features/random/domain/usecases/get_random.dart';
+import 'package:chuck_norris_io/features/random/presentation/bloc/random_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -19,4 +25,21 @@ void initAuthDependecies() {
 
 Future<void> initDependencies() async {
   getIt.pushNewScope();
+  await initRandom();
+}
+
+Future<void> initRandom() async {
+  getIt.registerLazySingleton(() => RandomBloc(
+        getRandomUseCase: getIt(),
+      ));
+
+  getIt.registerLazySingleton(() => GetRandomUseCase(repository: getIt()));
+
+  getIt.registerLazySingleton<RandomClient>(() => RandomClientImpl(
+        apiClient: getIt(),
+      ));
+
+  getIt.registerLazySingleton<RandomRepository>(() => RandomRepositoryImpl(
+        randomClient: getIt(),
+      ));
 }
