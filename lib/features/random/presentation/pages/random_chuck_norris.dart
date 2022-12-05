8e2323d4cf84beb3ff000aafa274b500811fec74
Max Herrera <harvey.herrera@lumation.co>
2022-12-05 00:16:38 -0600

    added ui implementation for get bloc instance

--- lib/features/random/presentation/pages/random_chuck_norris.dart
+++ lib/features/random/presentation/pages/random_chuck_norris.dart
@@ -1,16 +1,96 @@
+import 'package:chuck_norris_io/core/injection/injection_container.dart';
+import 'package:chuck_norris_io/features/random/domain/entities/random_entitie.dart';
+import 'package:chuck_norris_io/features/random/presentation/bloc/random_bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
-class RandomChuckNorrisScreen extends StatelessWidget {
+class RandomChuckNorrisScreen extends StatefulWidget {
   static const routeName = '/';
 
   const RandomChuckNorrisScreen({Key? key}) : super(key: key);
 
   @override
+  State<RandomChuckNorrisScreen> createState() =>
+      _RandomChuckNorrisScreenState();
+}
+
+class _RandomChuckNorrisScreenState extends State<RandomChuckNorrisScreen> {
+  RandomEntity randomEntity = const RandomEntity(
+    id: "",
+    url: "",
+    value: "",
+    iconUrl: "",
+    createdAt: "",
+    updatedAt: "",
+    categories: [],
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Procedo a disenar'),
+    final randomBloc = getIt<RandomBloc>();
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: BlocProvider<RandomBloc>(
+        create: (context) {
+          randomBloc.add(const GetRandomEvent());
+          return randomBloc;
+        },
+        child: BlocConsumer<RandomBloc, RandomState>(
+          listener: (context, state) {
+            if (state is SuccessGetRandomState) {
+              setState(() {
+                randomEntity = state.randomEntity;
+              });
+            }
+          },
+          builder: (context, state) {
+            return SafeArea(
+                child: state is LoadingGetRandomState
+                    ? _loading()
+                    : _principalBody());
+          },
+        ),
       ),
+    );
+  }
+
+  Widget _principalBody() {
+    return ListView(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      children: [
+        SizedBox(
+          height: 10.h,
+        ),
+        Image.network(
+          "https://avatars.githubusercontent.com/u/17794049?s=280&v=4",
+          width: 20.w,
+          height: 20.h,
+        ),
+        SizedBox(
+          height: 10.h,
+        ),
+        Text(
+          randomEntity.value,
+          textAlign: TextAlign.justify,
+          style: GoogleFonts.jost(
+            fontWeight: FontWeight.w800,
+            fontSize: 0.3.dp,
+          ),
+        ),
+        SizedBox(
+          height: 25.h,
+        ),
+      ],
+    );
+  }
+
+  Widget _loading() {
+    return const Center(
+      child: CupertinoActivityIndicator(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:chuck_norris_io/core/injection/injection_container.dart';
 import 'package:chuck_norris_io/features/main/common_main.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  initAuthDependecies();
+  await initDependencies();
   runApp(const MyApp());
 }
