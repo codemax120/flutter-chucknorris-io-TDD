00b87b9ec5017487938df459d84995d1f3db1b2f
Max Herrera <codemax120@gmail.com>
2023-08-12 11:55:39 -0600

    added class for mock connection app internet and integration test cases

--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -3,15 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-class MockNetworkInfoRepository extends Mock implements NetworkInfoRepository {
+class MockNetworkInfoRepositoryConnected extends Mock
+    implements NetworkInfoRepository {
   @override
   Future<bool> get hasConnection async => true;
+}
+
+class MockNetworkInfoRepositoryNoConnected extends Mock
+    implements NetworkInfoRepository {
+  @override
+  Future<bool> get hasConnection async => false;
 }
 
 void networkInfoTest() {
   group('NetworkInfoRepository', () {
     test('hasConnection returns true when connected', () async {
-      final networkInfoRepository = MockNetworkInfoRepository();
+      final networkInfoRepository = MockNetworkInfoRepositoryConnected();
 
       // Simulates that a connection is established
       final bool result = await networkInfoRepository.hasConnection;
@@ -20,18 +27,19 @@ void networkInfoTest() {
     });
 
     test('hasConnection returns false when not connected', () async {
-      final networkInfoRepository = MockNetworkInfoRepository();
+      final networkInfoRepository = MockNetworkInfoRepositoryNoConnected();
 
       // Simulates that not a connection is established
       final bool result = await networkInfoRepository.hasConnection;
 
-      expect(!result, false);
+      expect(result, false);
     });
   });
 
   group('NetworkInfoRepositoryImpl', () {
-    testWidgets('hasConnection returns false for no connectivity',
+    testWidgets('hasConnection returns true for no connectivity',
         (WidgetTester tester) async {
+      bool hasConnection = true;
       // Configure the integration test, create a MaterialApp
       await tester.pumpWidget(
         MaterialApp(
@@ -40,7 +48,7 @@ void networkInfoTest() {
               final networkInfoRepository = NetworkInfoRepositoryImpl();
               return ElevatedButton(
                 onPressed: () async {
-                  await networkInfoRepository.hasConnection;
+                  hasConnection = await networkInfoRepository.hasConnection;
                 },
                 child: const Text('Test Button'),
               );
@@ -52,6 +60,35 @@ void networkInfoTest() {
       // Performs actions that generate the use of NetworkInfoRepositoryImp
       await tester.tap(find.byType(ElevatedButton));
       await tester.pumpAndSettle();
+
+      expect(hasConnection, true);
+    });
+
+    testWidgets('hasConnection returns false for no connectivity',
+        (WidgetTester tester) async {
+      bool hasConnection = false;
+      // Configure the integration test, create a MaterialApp
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Builder(
+            builder: (context) {
+              final networkInfoRepository = NetworkInfoRepositoryImpl();
+              return ElevatedButton(
+                onPressed: () async {
+                  hasConnection = await networkInfoRepository.hasConnection;
+                },
+                child: const Text('Test Button'),
+              );
+            },
+          ),
+        ),
+      );
+
+      // Performs actions that generate the use of NetworkInfoRepositoryImp
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+
+      expect(hasConnection, false);
     });
   });
 }
