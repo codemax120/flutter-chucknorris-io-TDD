81467136b6611e0c0c913b0822f56c0d2b324a8d
Max Herrera <codemax120@gmail.com>
2023-08-12 10:41:11 -0600

    removed unused class from failure dart file

--- lib/core/network/failure.dart
+++ lib/core/network/failure.dart
@@ -2,19 +2,6 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {}
 
-class FailureError extends Failure {
-  final Object? error;
-
-  FailureError({
-    this.error,
-  });
-
-  @override
-  List<Object?> get props => [
-        error,
-      ];
-}
-
 class ServerFailure extends Failure {
   final Object? error;
 
