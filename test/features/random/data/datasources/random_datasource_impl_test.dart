0e1a7ab35b9a72a4c45016a17b780452aa0db622
Max Herrera <codemax120@gmail.com>
2023-08-12 23:30:22 -0600

    added expected response from unit test

--- test/features/random/data/datasources/random_datasource_impl_test.dart
+++ test/features/random/data/datasources/random_datasource_impl_test.dart
@@ -3,6 +3,7 @@ import 'package:chuck_norris_io/core/network/network_info.dart';
 import 'package:chuck_norris_io/core/network/server_api_client.dart';
 import 'package:chuck_norris_io/features/random/data/datasources/random_datasource_impl.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
 import 'package:nock/nock.dart';
 
 void randomDataSourceImplTest() {
@@ -23,9 +24,11 @@ void randomDataSourceImplTest() {
       final response = await randomClient.getRandom();
 
       expect(interceptor.isDone, true);
-      expect(response.statusCode, 200);
-      expect(response.body, "result");
+      expect(response, isA<Response>());
+      expect(response.statusCode, equals(200));
+      expect(response.body, equals("result"));
     });
+
     test('getRandom should handle error response', () async {
       final interceptor = nock.get("/jokes/random")
         ..reply(400, 'error message');
@@ -33,8 +36,9 @@ void randomDataSourceImplTest() {
       final response = await randomClient.getRandom();
 
       expect(interceptor.isDone, true);
-      expect(response.statusCode, 400);
-      expect(response.body, "error message");
+      expect(response, isA<Response>());
+      expect(response.statusCode, equals(400));
+      expect(response.body, equals("error message"));
     });
   });
 }
